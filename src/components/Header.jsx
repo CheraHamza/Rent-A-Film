ccca356b1d728d0295d9ce2886e8a8b07a2b2de8
CheraHamza <cherahamza1@gmail.com>
2025-07-27 16:28:29 +0100

    feat: create Header componet and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,28 @@
+import styled from "styled-components";
+
+import logo from "/Icons/logo.svg";
+import Navbar from "./Navbar";
+import CartIcon from "./CartIcon";
+
+const StyledHeader = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 5px 30px;
+`;
+
+const StyledLogo = styled.img`
+	width: 40px;
+	height: 40px;
+`;
+
 const Header = () => {
 	return (
-		<div>
-			<h1>this is the header</h1>
-		</div>
+		<StyledHeader>
+			<StyledLogo src={logo} alt="logo"></StyledLogo>
+			<Navbar></Navbar>
+			<CartIcon></CartIcon>
+		</StyledHeader>
 	);
 };
 
