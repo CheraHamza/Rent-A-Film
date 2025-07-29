f222b8632a0ee73d0a565a7d3a899fde9f2f0c38
CheraHamza <cherahamza1@gmail.com>
2025-07-29 12:24:01 +0100

    feat: add background movie frames to the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,54 @@
 import Header from "../components/Header";
+import styled from "styled-components";
+
+import bg_1 from "/Images/image_1.jpg";
+import bg_2 from "/Images/image_2.jpg";
+import bg_3 from "/Images/image_3.jpg";
+import bg_4 from "/Images/image_4.jpg";
+import bg_5 from "/Images/image_5.jpg";
+
+const StyledHomePage = styled.div`
+	width: 100%;
+	height: 100%;
+
+	position: relative;
+
+	.header {
+		position: absolute;
+		width: 100%;
+		z-index: 99;
+	}
+`;
+
+const FrameWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+`;
+
+const Frame = styled.div`
+	width: 100%;
+	height: 90vh;
+	background-image: url(${(props) => props.$backgroundImg});
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-color: #000000be;
+	background-blend-mode: darken;
+`;
 
 function HomePage() {
 	return (
-		<div>
+		<StyledHomePage>
 			<Header></Header>
-			<div>movies</div>
-		</div>
+			<FrameWrapper>
+				<Frame $backgroundImg={bg_1}></Frame>
+				<Frame $backgroundImg={bg_2}></Frame>
+				<Frame $backgroundImg={bg_3}></Frame>
+				<Frame $backgroundImg={bg_4}></Frame>
+				<Frame $backgroundImg={bg_5}></Frame>
+			</FrameWrapper>
+		</StyledHomePage>
 	);
 }
 
