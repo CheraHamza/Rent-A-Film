86065d2693624df46a9e1f20dfd8e065a6e59df8
CheraHamza <cherahamza1@gmail.com>
2025-07-29 15:52:02 +0100

    feat: add onclick prop to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -17,8 +17,8 @@ const StyledButton = styled.button`
 	}
 `;
 
-const Button = ({ text }) => {
-	return <StyledButton>{text}</StyledButton>;
+const Button = ({ text, onClick }) => {
+	return <StyledButton onClick={onClick}>{text}</StyledButton>;
 };
 
 export default Button;
