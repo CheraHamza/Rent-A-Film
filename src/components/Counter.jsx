3163f54951e4e50ef596d95c4e1a2cbf389821cb
CheraHamza <cherahamza1@gmail.com>
2025-08-02 18:29:02 +0100

    feat: create Counter component to use in setting amount of days to rent

---
+++ src/components/Counter.jsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { ChevronDown, ChevronUp } from "lucide-react";
+
+const StyledCounter = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const StyledButton = styled.button`
+	width: 25px;
+	height: 25px;
+	background-color: transparent;
+	border: none;
+	color: white;
+
+	cursor: pointer;
+
+	&.disabled {
+		cursor: not-allowed;
+	}
+
+	.lucide-chevron-up {
+		transform: translateY(3px);
+	}
+
+	.lucide-chevron-down {
+		transform: translateY(-3px);
+	}
+
+	&:hover .lucide {
+		transform: translateY(0px);
+	}
+
+	.lucide {
+		width: 100%;
+		height: 100%;
+
+		transition: all 0.15s ease-in-out;
+	}
+`;
+
+const StyledCount = styled.p`
+	font-family: "DM Serif Display";
+
+	font-size: 18px;
+`;
+
+const Counter = ({ count, setCount }) => {
+	const increment = () => {
+		if (count < 10) {
+			setCount(count + 1);
+		}
+	};
+
+	const decrement = () => {
+		if (count > 1) {
+			setCount(count - 1);
+		}
+	};
+
+	return (
+		<StyledCounter>
+			<StyledButton
+				className={count === 10 ? "disabled" : ""}
+				onClick={increment}
+			>
+				<ChevronUp />
+			</StyledButton>
+			<StyledCount>{count}</StyledCount>
+			<StyledButton
+				className={count === 1 ? "disabled" : ""}
+				onClick={decrement}
+			>
+				<ChevronDown />
+			</StyledButton>
+		</StyledCounter>
+	);
+};
+
+export default Counter;
