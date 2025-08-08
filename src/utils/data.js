79c0703d73926e09c55823c15c835926ea427433
CheraHamza <cherahamza1@gmail.com>
2025-08-08 15:15:14 +0100

    refactor: simplify data structure by removing unnecessary card_info nesting

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,32 +2,26 @@ const data = {
 	wishlist: [
 		{
 			id: 238,
-			card_info: {
-				title: "The Godfather",
-				date: "Mar 14, 1972",
-				rating: 8.69,
-				poster_url:
-					"https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-			},
+			title: "The Godfather",
+			date: "Mar 14, 1972",
+			rating: 8.7,
+			poster_url:
+				"https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
 		},
 	],
 
 	cart: [
 		{
 			id: 238,
-			card_info: {
-				title: "The Godfather",
-				date: "Mar 14, 1972",
-				rating: 8.69,
-				poster_url:
-					"https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-			},
+			title: "The Godfather",
+			year: 1972,
+			poster_url:
+				"https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+
+			days: 1,
+			price: 9.99,
 		},
 	],
 };
 
-function getWishlist() {
-	return data.wishlist;
-}
-
-export { data, getWishlist };
+export { data };
