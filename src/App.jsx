31cf2e2a95ed6bbe69353a7e7057fd718280b26f
CheraHamza <cherahamza1@gmail.com>
2025-08-14 08:44:21 +0100

    feat: implement catalog search functionality with dynamic query handling and state management

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,17 @@ const StyledApp = styled.div`
 
 const App = () => {
 	const [data, setData] = useState(initialData);
+	const [catalog, setCatalog] = useState([]);
+	const [search, setSearch] = useState("");
+	const [pages, setPages] = useState(1);
+	const [query, setQuery] = useState("");
+
+	const [filters, setFilters] = useState({
+		genres: [],
+		sort: "Alphabetical",
+		order: "Ascending",
+	});
+
 	const [movieDetailView, setMovieDetailView] = useState({
 		id: null,
 		title: null,
@@ -40,13 +51,44 @@ const App = () => {
 		}
 	}, [url]);
 
+	useEffect(() => {
+		const urlElements = url.split("/");
+		if (urlElements[0] === "catalog") {
+			if (urlElements[1]) {
+				const searchElement = urlElements[1].split("=");
+				if (searchElement[0] === "search") {
+					setSearch(searchElement[1]);
+				}
+			}
+		}
+	}, [url]);
+
+	useEffect(() => {
+		let query = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${pages}`;
+
+		setCatalog([]);
+		setPages(1);
+
+		if (search) {
+			query = `https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=${pages}`;
+		}
+		setQuery(query);
+	}, [search, pages]);
+
 	const pageContent =
 		url === "home" ? (
 			<HomePage />
-		) : url === "catalog" ? (
+		) : url === "catalog" || url === `catalog/search=${search}` ? (
 			<CatalogPage
 				data={data}
 				setData={setData}
+				catalog={catalog}
+				setCatalog={setCatalog}
+				pages={pages}
+				setPages={setPages}
+				query={query}
+				filters={filters}
+				setFilters={setFilters}
 				movieDetailView={movieDetailView}
 				setMovieDetailView={setMovieDetailView}
 			/>
