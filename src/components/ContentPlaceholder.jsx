a93813b490df7816a25aa8eff6dca6a99dcec364
CheraHamza <cherahamza1@gmail.com>
2025-08-22 18:36:07 +0100

    feat: add ContentPlaceholder component for improved loading state representation

---
+++ src/components/ContentPlaceholder.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const Placeholder = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 20px;
+
+	color: white;
+	opacity: 0.6;
+
+	.lucide {
+		width: 100px;
+		height: 100px;
+	}
+`;
+
+const ContentPlaceholder = ({ illustration, text }) => {
+	return (
+		<Placeholder>
+			{illustration}
+			<p>{text}</p>
+		</Placeholder>
+	);
+};
+
+export default ContentPlaceholder;
